14bd384f3d87118507a4a14e668742cbf8a3095a
Валентин Ковальчук <val-81@mail.ru>
2022-01-23 23:22:14 +0300

    Добавьте файлы проекта.

---
+++ task_WPF_07/CloseFileDialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_WPF_07
+{
+    internal class CloseFileDialog
+    {
+        public string Filter { get; internal set; }
+        public string FileName { get; internal set; }
+
+        internal bool ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
